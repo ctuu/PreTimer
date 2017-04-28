dbfa34da94cb87c7c3bd2c3f1dd550255ac37830
ctuu <ct.liuu@gmail.com>
2017-04-28 18:05:45 +0800

    UI changed

--- PreTimer/PreTimer/MainWindow.xaml.cs
+++ PreTimer/PreTimer/MainWindow.xaml.cs
@@ -22,27 +22,11 @@ namespace PreTimer
             D_Timer.Tick += new EventHandler(D_Timer_Tick);
             D_Timer.Interval = new TimeSpan(0, 0, 1);
             tb_time.Text = set_time.ToString("mm:ss");
-            tb_Min.Text = Convert.ToString(Properties.Settings.Default.D_min);
-            tb_Sec.Text = Convert.ToString(Properties.Settings.Default.D_sec);
-            tb_tips_mu.Text = Properties.Settings.Default.D_tips_mu;
-            tb_over_mu.Text = Properties.Settings.Default.D_over_mu;
             f_tips_mu.Open(new Uri(Properties.Settings.Default.D_tips_mu));
             f_over_mu.Open(new Uri(Properties.Settings.Default.D_over_mu));
         }
 
-        private void Btu_Setting_save_Click(object sender, RoutedEventArgs e)
-        {
-            Properties.Settings.Default.D_min = Convert.ToInt32(tb_Min.Text);
-            Properties.Settings.Default.D_sec = Convert.ToInt32(tb_Sec.Text);
-            Properties.Settings.Default.D_tips_min = Convert.ToInt32(tb_tips_Min.Text);
-            Properties.Settings.Default.D_tips_sec = Convert.ToInt32(tb_tips_Sec.Text);
-            Properties.Settings.Default.Save();
-            if (!isR && isTO)
-            {
-                set_time = new DateTime(1, 1, 1, 0, Properties.Settings.Default.D_min, Properties.Settings.Default.D_sec);
-                tb_time.Text = set_time.ToString("mm:ss");
-            }
-        }
+        
         private void D_Timer_Tick(object sender, EventArgs e)
         {
 
@@ -98,74 +82,6 @@ namespace PreTimer
             btu_Run.Content = "Run";
         }
 
-        private void Btu_tips_mu_load_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog tips_mu = new Microsoft.Win32.OpenFileDialog()
-            {
-                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
-                DefaultExt = "*.mp3;*.wav",
-                Filter = "*|.mp3;*.wav"
-            };
-            if (tips_mu.ShowDialog() == true)
-            {
-                f_tips_mu.Close();
-                tb_tips_mu.Text = tips_mu.FileName;
-                Properties.Settings.Default.D_tips_mu = tips_mu.FileName;
-                Properties.Settings.Default.Save();
-                f_tips_mu.Open(new Uri(Properties.Settings.Default.D_tips_mu));
-            }
-        }
-
-        private void Btu_tips_mu_test_Click(object sender, RoutedEventArgs e)
-        {
-            if (!System.IO.File.Exists(Properties.Settings.Default.D_tips_mu))
-            {
-                MessageBox.Show("File not exist.");
-            }
-            else
-            {
-                f_tips_mu.Stop();
-                f_tips_mu.Play();
-            }
-        }
-
-        private void Btu_over_mu_load_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog over_mu = new Microsoft.Win32.OpenFileDialog()
-            {
-                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
-                DefaultExt = "*.mp3;*.wav",
-                Filter = "*|.mp3;*.wav"
-            };
-            if (over_mu.ShowDialog() == true)
-            {
-                f_over_mu.Close();
-                tb_over_mu.Text = over_mu.FileName;
-                Properties.Settings.Default.D_over_mu = over_mu.FileName;
-                Properties.Settings.Default.Save();
-                f_over_mu.Open(new Uri(Properties.Settings.Default.D_over_mu));
-            }
-        }
-
-        private void Btu_over_mu_test_Click(object sender, RoutedEventArgs e)
-        {
-            if (!System.IO.File.Exists(Properties.Settings.Default.D_over_mu))
-            {
-                MessageBox.Show("File not exist.");
-            }
-            else
-            {
-                f_over_mu.Stop();
-                f_over_mu.Play();
-            }
-        }
-
-        private void Btu_about_Click(object sender, RoutedEventArgs e)
-        {
-            About a = new About();
-            a.ShowDialog();
-        }
-
         private void btu_exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -173,8 +89,18 @@ namespace PreTimer
 
         private void Btu_setting_Click(object sender, RoutedEventArgs e)
         {
+            f_tips_mu.Close();
+            f_over_mu.Close();
             Setting a = new Setting();
             a.ShowDialog();
+            if (!isR && isTO)
+            {
+                set_time = new DateTime(1, 1, 1, 0, Properties.Settings.Default.D_min, Properties.Settings.Default.D_sec);
+                tb_time.Text = set_time.ToString("mm:ss");
+            }
+            f_tips_mu.Open(new Uri(Properties.Settings.Default.D_tips_mu));
+            f_over_mu.Open(new Uri(Properties.Settings.Default.D_over_mu));
+
         }
     }
 }
--- PreTimer/PreTimer/Setting.xaml.cs
+++ PreTimer/PreTimer/Setting.xaml.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PreTimer
 {
@@ -18,9 +8,88 @@ namespace PreTimer
     /// </summary>
     public partial class Setting : Window
     {
+        private System.Windows.Media.MediaPlayer f_tips_mu = new System.Windows.Media.MediaPlayer();
+        private System.Windows.Media.MediaPlayer f_over_mu = new System.Windows.Media.MediaPlayer();
         public Setting()
         {
             InitializeComponent();
+            tb_Min.Text = Convert.ToString(Properties.Settings.Default.D_min);
+            tb_Sec.Text = Convert.ToString(Properties.Settings.Default.D_sec);
+            tb_tips_mu.Text = Properties.Settings.Default.D_tips_mu;
+            tb_over_mu.Text = Properties.Settings.Default.D_over_mu;
+        }
+
+        private void Btu_Setting_save_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.D_min = Convert.ToInt32(tb_Min.Text);
+            Properties.Settings.Default.D_sec = Convert.ToInt32(tb_Sec.Text);
+            Properties.Settings.Default.D_tips_min = Convert.ToInt32(tb_tips_Min.Text);
+            Properties.Settings.Default.D_tips_sec = Convert.ToInt32(tb_tips_Sec.Text);
+            Properties.Settings.Default.Save();
+        }
+        private void Btu_tips_mu_load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog tips_mu = new Microsoft.Win32.OpenFileDialog()
+            {
+                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                DefaultExt = "*.mp3;*.wav",
+                Filter = "*|.mp3;*.wav"
+            };
+            if (tips_mu.ShowDialog() == true)
+            {
+                tb_tips_mu.Text = tips_mu.FileName;
+                Properties.Settings.Default.D_tips_mu = tips_mu.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Btu_tips_mu_test_Click(object sender, RoutedEventArgs e)
+        {
+            if (!System.IO.File.Exists(Properties.Settings.Default.D_tips_mu))
+            {
+                MessageBox.Show("File not exist.");
+            }
+            else
+            {
+                f_tips_mu.Stop();
+                f_tips_mu.Play();
+            }
+        }
+
+        private void Btu_over_mu_load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog over_mu = new Microsoft.Win32.OpenFileDialog()
+            {
+                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                DefaultExt = "*.mp3;*.wav",
+                Filter = "*|.mp3;*.wav"
+            };
+            if (over_mu.ShowDialog() == true)
+            {
+                tb_over_mu.Text = over_mu.FileName;
+                Properties.Settings.Default.D_over_mu = over_mu.FileName;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Btu_over_mu_test_Click(object sender, RoutedEventArgs e)
+        {
+            if (!System.IO.File.Exists(Properties.Settings.Default.D_over_mu))
+            {
+                MessageBox.Show("File not exist.");
+            }
+            else
+            {
+                f_over_mu.Stop();
+                f_over_mu.Play();
+            }
+        }
+
+        private void Btu_about_Click(object sender, RoutedEventArgs e)
+        {
+            About a = new About();
+            a.ShowDialog();
         }
     }
+
 }
